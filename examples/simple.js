35cceed4e0a287fd7594c68cde291814f6aaea8b
Gorilla's Habitat <gorilla@creaselabs.io>
2023-07-11 16:32:34 -0700

    Updating example

--- examples/simple.js
+++ examples/simple.js
@@ -2,14 +2,26 @@
  * This is an example of the most barebones implementation of a Curator
  **/
 
-import {Curator} from '@open-oracle-origami/origami-js-sdk';
-import {CoinbaseMill} from "@open-oracle-origami/origami-js-mill-coinbase";
-import {ConsoleMuseum} from "@open-oracle-origami/origami-js-museum-console";
+import { Curator } from '@open-oracle-origami/origami-js-sdk'
+import { CoinbaseMill } from '@open-oracle-origami/origami-js-mill-coinbase'
+import { CoinMarketCapMill } from '@open-oracle-origami/origami-js-mill-coinmarketcap'
+import { CoinGeckoMill } from '@open-oracle-origami/origami-js-mill-coingecko'
+import { ConsoleMuseum } from '@open-oracle-origami/origami-js-museum-console'
 
-
-const curator = new Curator();
-curator.addMill(CoinbaseMill, {pairs: ['BTC-USDT']})
-curator.addMuseum(ConsoleMuseum, {id: 'console', sources: ['mill.coinbase']})
-
+const curator = new Curator()
+curator.addMill(CoinbaseMill, { pairs: ['BTC-USDT'] })
+curator.addMill(CoinMarketCapMill, {
+  apiKey: process.env.COINMARKETCAP_API_KEY,
+  getQuotesParams: { symbol: 'BTC' },
+  pollIntervalMs: 5000,
+})
+curator.addMill(CoinGeckoMill, {
+  simplePriceParams: { ids: '1inch', vs_currencies: 'usd' },
+  pollIntervalMs: 5000,
+})
+curator.addMuseum(ConsoleMuseum, {
+  id: 'console',
+  sources: ['mill.coingecko', 'mill.coinmarketcap'],
+})
 
 curator.start()
